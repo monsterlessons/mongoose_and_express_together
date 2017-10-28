26d95e8a045f44d12825f1e8867420957441928a
Alexander Kochergin <ejiqpep@gmail.com>
2017-10-28 20:09:01 +0200

    Added mongoose and express together

--- index.js
+++ index.js
@@ -1,22 +1,54 @@
+const express = require('express')
 const mongoose = require('mongoose')
 const User = require('./models/user')
-mongoose.Promise = require('bluebird')
 
-mongoose.connect('mongodb://localhost/testmongoose', {
-  useMongoClient: true
-})
-
-const db = mongoose.connection
-
-db.on('error', err => {
-  console.log('error connection', err)
-})
-
-db.once('open', () => {
+const app = express()
+app.get('/user', (req, res) => {
   User.findUserByName('alex', (err, user) => {
-    console.log(err, user)
-    user.findSimilarUsersByCountry((err, users) => {
-      console.log(err, users)
-    })
+    res.json(user)
   })
 })
+
+const port = 3999
+const startServer = () => {
+  app.listen(port)
+  console.log(`App started on port ${port}`)
+}
+const connectDb = () => {
+  mongoose.Promise = require('bluebird')
+
+  const options = {
+    useMongoClient: true
+  }
+
+  mongoose.connect('mongodb://localhost/testmongoose', options)
+  return mongoose.connection
+}
+
+connectDb()
+  .on('error', console.log)
+  .on('disconnected', connectDb)
+  .once('open', startServer)
+
+// const mongoose = require('mongoose')
+// const User = require('./models/user')
+// mongoose.Promise = require('bluebird')
+//
+// mongoose.connect('mongodb://localhost/testmongoose', {
+//   useMongoClient: true
+// })
+//
+// const db = mongoose.connection
+//
+// db.on('error', err => {
+//   console.log('error connection', err)
+// })
+//
+// db.once('open', () => {
+//   User.findUserByName('alex', (err, user) => {
+//     console.log(err, user)
+//     user.findSimilarUsersByCountry((err, users) => {
+//       console.log(err, users)
+//     })
+//   })
+// })
